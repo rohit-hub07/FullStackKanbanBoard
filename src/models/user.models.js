3d5718dc3170294b8b832d8cd1560b5ece88b8e3
rohit singh <rohitsingh986491@gmail.com>
2025-04-02 03:21:41 +0530

    added prehook and some methods to user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,54 +1,104 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
-const userSchema = new Schema({
-  avatar: {
-    type: {
-      url: String,
-      localpath: String,
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localpath: String,
+      },
+      default: {
+        url: `https://placehold.co/600x400`,
+        localpath: "",
+      },
     },
-    default: {
-      url: `https://placehold.co/600x400`,
-      localpath: "",
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerifed: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: String,
+    },
+    emailVerifiedToken: {
+      type: String,
+    },
+    emailVerifiedExpiry: {
+      type: String,
     },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  isEmailVerifed: {
-    type: Boolean,
-    default: false,
-  },
-  refreshToken: {
-    type: String,
-  },
-  forgotPasswordToken: {
-    type: String,
-  },
-  forgotPasswordExpiry: {
-    type: String,
-  },
-  emailVerifiedToken: {
-    type: String,
-  },
-  emailVerifiedExpiry: {
-    type: String,
-  },
-}, { timestamps: true})
+  { timestamps: true },
+);
+
+userSchema.pre("password", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.method.isPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.SECRET_ACCES_TOKEN,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.method.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.SECRET_REFRESH_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.method.generateTemporaryToken = function(){
+  const unhashed = crypto.randomBytes(32).toString("hex");
+  const hashed = crypto.createHash("sha20").update(unhashed).digest("hex");
+  const tokenExpiry = Date.now() + (20 * 60 * 1000)
+
+  return { unhashed, hashed, tokenExpiry}
+}
 
 export const User = mongoose.model("User", userSchema);
